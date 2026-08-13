77f04e2b2d5fc9ec58e6a6981445a23ec8d4925a
desmatriculandos <rm98870@estudante.fieb.edu.br>
2026-08-13 11:53:05 -0300

    add validação dos campos de cadastro com express-validator

--- app/routes/router.js
+++ app/routes/router.js
@@ -24,7 +24,34 @@ router.post("/login", (req, res) => {
 });
 
 
-router.get("/cadastro", (req, res) => {
+router.get("/cadastro", [
+
+    body("nome")
+    .notEmpty().withMessage("O campo nome é obrigatório")
+    .isLength({ min: 3 }).withMessage("O campo nome deve ter no mínimo 3 caracteres")
+    .trim(),
+
+    body("email")
+    .notEmpty().withMessage("O campo email é obrigatório")
+    .isEmail().withMessage("O campo email deve ser um email válido")
+    .normalizeEmail(),
+
+    body("senha")
+    .notEmpty().withMessage("O campo senha é obrigatório")
+    .isLength({ min: 6 }).withMessage("O campo senha deve ter no mínimo 6 caracteres")
+    .matches(/[A-Z]/).withMessage("O campo senha deve conter pelo menos uma letra maiúscula")
+    .matches(/[0-9]/).withMessage("O campo senha deve conter pelo menos um número"),
+
+    body("confirmarSenha")
+    .notEmpty().withMessage("O campo confirmar senha é obrigatório")
+    .custom((value, { req }) => {
+        if (value !== req.body.senha) {
+            throw new Error("As senhas não coincidem");
+        }
+        return true;
+    })
+
+], (req, res) => {
     res.render("pages/cadastro");
 });
 
